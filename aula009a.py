35faa9f393b96b6f0cdb38b9251d657d82dfe4a3
FabricioSousa-dev <ffabricioteixeira15@gmail.com>
2026-03-02 15:06:26 -0300

    Adição

--- aula009a.py
+++ aula009a.py
@@ -1,9 +1,27 @@
 #frase = "café"
-frase = "paralelepipedo"
+frase = "paralelepipedo de medida baixa"
 #print(frase[3])
 #print(frase[0:4])
 #print(frase[0:15:2])
 #print(frase[:5])
 #print(frase[0:])
-#print(frase[1::3])
+#print
+#len(frase)
+#print(len(frase))
+#frase.count("o")
+#frase.count("a",0,13)
+#frase.find("lele")
+#'para' in frase
+#frase.replace("paralelepipedo","Quadrado")
+#frase.upper()
+#frase.lower()
+#frase.capitalize()
+#frase.title()
+#frase.strip()
+#frase.rstrip()
+#frase.lstrip
+#frase.split()
+#'-'.join(frase)
+print()
+
 
