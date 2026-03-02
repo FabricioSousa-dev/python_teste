0b1ff8cc4210d408c46cfcbd1c8a2c501ef70ec2
FabricioSousa-dev <ffabricioteixeira15@gmail.com>
2026-03-02 13:51:13 -0300

    Mais exercicios

--- aula009a.py
+++ aula009a.py
@@ -1,2 +1,9 @@
-frase = "café"
-print(frase[3])
+#frase = "café"
+frase = "paralelepipedo"
+#print(frase[3])
+#print(frase[0:4])
+#print(frase[0:15:2])
+#print(frase[:5])
+#print(frase[0:])
+#print(frase[1::3])
+
