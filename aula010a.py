a9c83057e17da7b52844b54a920fa4ec0aeca618
FabricioSousa-dev <ffabricioteixeira15@gmail.com>
2026-03-26 14:27:57 -0300

    more exemples

---
+++ aula010a.py
@@ -0,0 +1,9 @@
+temp = int(input("your car yem how old? "))
+
+if temp <=3:
+    print("too young")
+else:
+    print("too old")
+
+
+    print("__end__")
---
+++ aula010b.py
@@ -0,0 +1,6 @@
+temp = int(input("Your car yem how old? "))
+
+print("Car young" if temp<=3 else "Car old")
+
+print("__end__")
+
